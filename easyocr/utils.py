9dc00fd3f4d58660f6719d1662a30c14350828d4
Boris Nikitin <aktzbn@gmail.com>
2023-01-27 14:18:36 +0300

    Use 3rd-party wordbeamsearch

--- easyocr/utils.py
+++ easyocr/utils.py
@@ -12,6 +12,9 @@ import sys, os
 from zipfile import ZipFile
 from .imgproc import loadImage
 
+# from ctc_decoder import beam_search as beam_search2, LanguageModel
+from word_beam_search import WordBeamSearch
+
 if sys.version_info[0] == 2:
     from six.moves.urllib.request import urlretrieve
 else:
@@ -307,6 +310,34 @@ class CTCLabelConverter(object):
                 dict_list[lang] = word_count
 
         self.dict_list = dict_list
+        lm_text = ' '.join(word_count)
+        #self.lm = LanguageModel(lm_text, self.character)
+
+# Parameters of the constructor of the `WordBeamSearch` class:
+# * Beam Width (beam_width): number of beams which are kept per time-step
+# * Scoring mode (lm_type): pass one of the four strings (not case-sensitive). The runtime with respect to the dictionary size W is given.
+#     * "Words": only use dictionary, no scoring: O(1)
+#     * "NGrams": use dictionary and score beams with LM: O(log(W))
+#     * "NGramsForecast": forecast (possible) next words and apply LM to these words: O(W*log(W))
+#     * "NGramsForecastAndSample": restrict number of (possible) next words to at most 20 words: O(W)
+# * Smoothing (lm_smoothing): LM uses add-k smoothing to allow word pairs which are not known from the training text, i.e. for which the bigram probability is zero. Set to values between 0 and 1, e.g. 0.01. To disable smoothing, set to 0
+# * Text (corpus): is given as a UTF8 encoded string. The operation creates its dictionary and (optionally) LM from it
+# * Characters (chars): is given as a UTF8 encoded string. If the number of characters is C, then the RNN output must have the size TxBx(C+1) with the last entry representing the CTC-blank label. The ordering of the characters must correspond to the ordering in the RNN output, e.g. if the RNN outputs the probabilities for "a", "b", " " and CTC-blank in this order, then the string "ab " must be passed
+# * Word characters (word_chars): is given as a UTF8 encoded string. Define how the algorithm extracts words from the text. If the word characters are "ab", and the text "aa ab bbb a" is passed, then the words "aa", "ab" and "bbb" will be extracted and used for the dictionary and the LM. To be able to recognize multiple words (e.g. a text-line), the word characters must be a subset of the characters recognized by the RNN (i.e. there must be at least one word-separating character like the space character): ```0<len(wordChars)<len(chars)```. In case only single words have to be detected, there is no need for a separating character, therefore the two parameters may also be equal: ```0<len(wordChars)<=len(chars)```
+
+# Input to the `WordBeamSearch.compute` method:
+# * Input matrix (mat)
+#   * numpy array
+#   * shape TxBx(C+1)
+#   * T is the number of time-steps, B the number of batch elements and C the number of characters
+#   * softmax-function already applied
+#   * CTC-blank must be the last entry along the character dimension in the matrix
+
+        beam_width = 20
+        corpus = lm_text
+        self.chars = ''.join(dict_character)
+        word_chars = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+        self.wbs = WordBeamSearch(beam_width, 'Words', 0.0, corpus.encode('utf8'), self.chars.encode('utf8'), word_chars.encode('utf8'))
 
     def encode(self, text, batch_max_length=25):
         """convert text-label into text-index.
@@ -347,6 +378,35 @@ class CTCLabelConverter(object):
         for i in range(mat.shape[0]):
             t = ctcBeamSearch(mat[i], self.character, self.ignore_idx, None, beamWidth=beamWidth)
             texts.append(t)
+        return texts
+
+    # def decode_beamsearch(self, mat, beamWidth=5):
+    # TODO: move blank symbol to end
+    #     texts = []
+    #     for i in range(mat.shape[0]):
+    #         t = beam_search2(mat[i], self.character, beam_width=beamWidth, lm=self.lm)
+    #         texts.append(t.replace('[blank]', ''))
+    #     return texts
+
+    def decode_wordbeamsearch(self, mat, beamWidth=5):
+        texts = []
+
+        # shape TxBx(C+1)
+        # T is the number of time-steps, B the number of batch elements and C the number of characters
+        # CTC-blank must be the last entry along the character dimension in the matrix
+
+        mat1 = np.swapaxes(mat, 0, 1)
+        perm = [z + 1 for z in range(mat.shape[2] - 1)]
+        perm.append(0)
+
+        mat2 = mat1[:, :, perm]
+
+        label_strs = self.wbs.compute(mat2)
+
+        for curr_label_str in label_strs:
+            s = ''.join([self.chars[label] for label in curr_label_str])
+            texts.append(s)
+
         return texts
 
     def decode_wordbeamsearch(self, mat, beamWidth=5):
