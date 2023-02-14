40ba2002c4b52c32e9d10789066d9d617012d035
Boris Nikitin <aktzbn@gmail.com>
2023-02-14 13:22:28 +0300

    Some change in trainer

--- trainer/train.py
+++ trainer/train.py
@@ -257,8 +257,8 @@ def train(opt, show_number = 2, amp=False):
                 head = f'{"Ground Truth":25s} | {"Prediction":25s} | Confidence Score & T/F'
                 predicted_result_log = f'{dashed_line}\n{head}\n{dashed_line}\n'
                 
-                #show_number = min(show_number, len(labels))
-                
+                show_number = min(show_number, len(labels))
+                #print('len(labels)', len(labels), labels)
                 start = random.randint(0,len(labels) - show_number )    
                 for gt, pred, confidence in zip(labels[start:start+show_number], preds[start:start+show_number], confidence_score[start:start+show_number]):
                     if 'Attn' in opt.Prediction:
