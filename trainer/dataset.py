5e6139c23cd821ba8e85708b4fd825698b11014a
Boris Nikitin <aktzbn@gmail.com>
2023-04-20 20:42:29 +0300

    Fix reading csv

--- trainer/dataset.py
+++ trainer/dataset.py
@@ -147,7 +147,7 @@ class OCRDataset(Dataset):
         self.root = root
         self.opt = opt
         print(root)
-        self.df = pd.read_csv(os.path.join(root,'labels.csv'), sep='^([^,]+),', engine='python', usecols=['filename', 'words'], keep_default_na=False)
+        self.df = pd.read_csv(os.path.join(root,'labels.csv'), sep=',', engine='python', usecols=['filename', 'words'], keep_default_na=False, escapechar='\\')
         self.nSamples = len(self.df)
 
         if self.opt.data_filtering_off:
